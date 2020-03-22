b94de77912e37bbb08631f91363226cdda3173fb
Rafael Guerlak <guerlak@hotmail.com>
2020-03-21 21:02:18 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Teste Guerlak</Text>
+    </>
+  );
+};
+
+export default App;
